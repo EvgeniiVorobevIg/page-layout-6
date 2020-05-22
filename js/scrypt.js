bb96f46ce7165d7098754a3ace256024125d024b
wayoutl1 <63786156+wayoutl1@users.noreply.github.com>
2020-05-22 11:52:27 +0500

    add22.07

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('.burger').click(function (event) {
         $('.burger, .block__menu-list').toggleClass('active');
+        $('body').toggleClass('lock');
     });
 });
-
